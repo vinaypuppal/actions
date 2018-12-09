4c976516e34ffb6bb2333f25f41cd95a95f0fb0b
vínαч puppαl <me@vinaypuppal.com>
2018-12-09 14:15:28 +0530

    Try with webhooks

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,28 +4,16 @@ if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
   echo "$BUILD_DIR didn't change"
 else
   echo "Deploying"
-  echo "Creating args.json"
-  jq \
-    --arg cmd "$NETLIFY_CMD" \
-    --arg base "$NETLIFY_BASE" \
-    --arg dir "$NETLIFY_DIR" \
-    --arg site_id "$NETLIFY_SITE_ID" \
-    '. + {cmd: $cmd, base: $base, dir: $dir, site_id: $site_id}' \
-     "$GITHUB_EVENT_PATH" > args.json
-  echo "Created args.json"
   echo "Sending build request"
   code=$(curl \
     --silent \
     --show-error \
     --output /dev/stderr \
     --write-out "%{http_code}" \
-    -H "Authorization: $GITHUB_TOKEN" \
-    -H "X-GitHub-Event: $GITHUB_EVENT_TYPE" \
     -H 'Content-Type: application/json' \
-    --data-binary @args.json \
-    "https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build"
+    -X POST -d '{}' \
+    "$NETLIFY_HOOK"
   ) 2>&1
-  echo "URL: https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build"
   echo "Status code $code"
   if [ ! 204 -eq "$code" ] && [ ! 200 -eq "$code" ]; then
     exit 1
